1ab20c5414cd585a02bcf22b4d0edce87b61f860
CKURZ <ckurz098@gmail.com>
2022-02-01 13:19:54 -0800

    Updated seeds.sql to add more catagories

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,22 +2,28 @@ INSERT INTO department (name)
 VALUES
 ('Sales'),
 ('Legal'),
-('Engineering');
+('Engineering'),
+('Internship');
 
 INSERT INTO role (title, salary,department_id)
 VALUES
+('Sales Manager', 120000, 1), 
 ('Saleperson',50000, 1),
-('Lawyer', 150000, 2),
-('Senior Engineer', 180000, 3)
-('Engineer', 90000, 3);
+('Chief Legal Officer', 200000, 2),
+('Lawyer', 130000, 2),
+('Senior Engineer', 180000, 3),
+('Engineer', 90000, 3),
+('Intern', 0 , 4);
 
 INSERT INTO employee (first_name, last_name,role_id,manager_id)
 VALUES
-('Billy', 'Bob', 1, NULL),
-('Bobo', 'Clown', 2, NULL),
-('June', 'Jasper', 3, NULL),
-('Quin', 'Hardin', 3,NULL),
-('Alvin', 'Crawford', 3,NULL),
-('Sam', 'Kenny', 2, NULL),
-('Orson', 'Brooks', 1, NULL),
-('Rees', 'Noble', 1, NULL);
+('Rees', 'Noble', 1, NULL),
+('Billy', 'Bob', 2, 1),
+('Orson', 'Brooks', 2, 1),
+('Bobo', 'Clown', 3, NULL),
+('Sam', 'Kenny', 4, 4),
+('June', 'Jasper', 5, NULL),
+('Quin', 'Hardin', 6, 6),
+('Alvin', 'Crawford', 6, 6),
+('Ben', 'Aflac', 7, 4), 
+('Greg', 'Wilf', 7, 6); 
